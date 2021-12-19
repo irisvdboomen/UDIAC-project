5c369c04a577d215ad1c345bec3bb2568b791bbd
Katrin438 <katq_sama@abv.bg>
2021-12-19 17:00:41 +0200

    catherine final changes

--- js/pop-up-reward.js
+++ js/pop-up-reward.js
@@ -1,34 +1,16 @@
 var Alert = new CustomAlert();
 
 function CustomAlert() {
-    this.render = function() {
+    this.render = function(event) {
         //Show Modal
-        let popUpBox = document.getElementById('popUpBox');
+        const popUpBox = event.previousElementSibling;
         popUpBox.style.display = "block";
         //Close Modal
-        // document.getElementById('closeModal').innerHTML = '<button onclick="Alert.ok()">OK</button>';
     }
 
-
-    this.ok = function() {
-        document.getElementById('popUpBox').style.display = "none";
-        document.getElementById('popUpOverlay').style.display = "none";
+    //closes the modals by using closest element
+    this.ok = function(event) {
+        event.closest('#popUpBox').style.display = "none";
+        event.closest('#popUpOverlay').style.display = "none";
     }
 }
-
-
-// profile remove claimed rewards:
-
-
-// document.querySelector(".confirm").addEventListener("click",(event)=>{
-//     event.target.closest(".rewards").remove();
-// })
-document.querySelector(".confirm-1").addEventListener("click", (event) => {
-    event.target.closest(".rewards-1").remove();
-})
-document.querySelector(".confirm-2").addEventListener("click", (event) => {
-    event.target.closest("rewards-2").remove();
-})
-document.querySelector(".confirm-3").addEventListener("click", (event) => {
-    event.target.closest(".rewards-3").remove();
-})
