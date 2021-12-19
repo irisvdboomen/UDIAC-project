dc52f72f9357846ee2a57002fba361fc44ef3639
umberto.a.semeleer <92313592+UmbertoAdrianSemeleer@users.noreply.github.com>
2021-12-19 21:56:27 +0100

    finished database

--- checkPoints.php
+++ checkPoints.php
@@ -1,12 +1,16 @@
 <?php
+session_start();
 include ("conection.php");
+ $customerID=$_SESSION['customerID'];
     $sql = "SELECT  SUM(`product`.`points`) as loyalty_points
     FROM ((`order` 
     INNER join `orderproduct` on `order`.`orderID` = `orderproduct`.`orderID`
-          INNER join `product` on `orderproduct`.`productID` = `product`.`productID`  ))
+          INNER join `product` on `orderproduct`.`productID` = `product`.`productID`))  
     where `order`.`customerID` = '$_SESSION['customerID']'";
     
-    $run_Sql = mysqli_query($db_connection, $sql);
-    echo "$run_Sql";
+  $result=mysqli_query($db_connection, $sql);
+
+ $row = mysqli_fetch_array($result);
+ // echo "$row";
 
 ?>
